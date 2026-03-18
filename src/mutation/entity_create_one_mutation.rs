d37359c66663c871796b2d2c3b9e7781f869cfd2
Vito Limandibhrata <vito@virtuala.app>
2026-03-18 08:09:00 +0800

    feat: handle custom null values

--- src/mutation/entity_create_one_mutation.rs
+++ src/mutation/entity_create_one_mutation.rs
@@ -163,9 +163,57 @@ where
         }
 
         match data.remove(&entity_object_builder.column_name::<T>(&column)) {
-            Some(value) => {
-                active_model.try_set(column, value)?;
-            }
+            Some(value) => match value {
+                sea_orm::Value::String(string_opt) => {
+                    if let Some(string_value) = string_opt {
+                        if (string_value.as_str() == "___-999999___") {
+                            active_model.try_set(column, sea_orm::Value::String(None))?;
+                        } else {
+                            active_model
+                                .try_set(column, sea_orm::Value::String(Some(string_value)))?;
+                        }
+                    }
+                }
+                sea_orm::Value::Int(numeric_value_opt) => {
+                    if let Some(numeric_value) = numeric_value_opt {
+                        if (numeric_value == -999999) {
+                            active_model.try_set(column, sea_orm::Value::Int(None))?;
+                        } else {
+                            active_model.try_set(column, value)?;
+                        }
+                    }
+                }
+                sea_orm::Value::BigInt(numeric_value_opt) => {
+                    if let Some(numeric_value) = numeric_value_opt {
+                        if (numeric_value == -999999) {
+                            active_model.try_set(column, sea_orm::Value::BigInt(None))?;
+                        } else {
+                            active_model.try_set(column, value)?;
+                        }
+                    }
+                }
+                sea_orm::Value::Float(numeric_value_opt) => {
+                    if let Some(numeric_value) = numeric_value_opt {
+                        if (numeric_value == -999999.0) {
+                            active_model.try_set(column, sea_orm::Value::Float(None))?;
+                        } else {
+                            active_model.try_set(column, value)?;
+                        }
+                    }
+                }
+                sea_orm::Value::Double(numeric_value_opt) => {
+                    if let Some(numeric_value) = numeric_value_opt {
+                        if (numeric_value == -999999.0) {
+                            active_model.try_set(column, sea_orm::Value::Double(None))?;
+                        } else {
+                            active_model.try_set(column, value)?;
+                        }
+                    }
+                }
+                _ => {
+                    active_model.try_set(column, value)?;
+                }
+            },
             None => continue,
         }
     }
